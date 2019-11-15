c87c30e6fa418d0be17207e085a5e6cb967315ae
Alexander Brodko <alexander.brodko@playrix.com>
2019-11-16 01:20:50 +0300

    Сделали с Андреем самую простую версию

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-	fetch(`/api/data?round_id=1&page=2&sort=-judge_rating_sum`)
-	.then(resp => resp.json())
-	.then(resp => console.log(resp))
-	.catch(err => console.error(err))
-	
+let n = 1;
+function GetNextTrack() {
+	return n++;
+}
+
+function App(props) {
+	const [src, setSrc] = useState('https://ib17.hip-hop.ru//upload/music/ib17_r1_Iamjmarr_d8b5f17.mp3');
+
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+			<audio controls autoPlay id="audio" src={src} onEnded={
+				_ => {
+					fetch('/api/trackinfo?id=' + GetNextTrack())
+						.then(info => info.json())
+						.then(data => {
+							setSrc('https://ib17.hip-hop.ru/' + data.path);
+						});
+				}
+			}>
+			</audio>
 		</div>
 	);
 }
